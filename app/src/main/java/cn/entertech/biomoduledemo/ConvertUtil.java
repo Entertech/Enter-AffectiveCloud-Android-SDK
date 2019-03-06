444cd29184c575d2e606e452f32c08ee94947212
lloyd <651519531@qq.com>
2019-03-06 11:45:23 +0800

    去掉多余日志

--- app/src/main/java/cn/entertech/biomoduledemo/ConvertUtil.java
+++ app/src/main/java/cn/entertech/biomoduledemo/ConvertUtil.java
@@ -1,10 +1,7 @@
 package cn.entertech.biomoduledemo;
 
-import android.util.Log;
-
 public class ConvertUtil {
     public static int converUnchart(byte data) {
-        Log.d("###","unchar is "+(data & 0xff));
         return (data & 0xff);
     }
 }

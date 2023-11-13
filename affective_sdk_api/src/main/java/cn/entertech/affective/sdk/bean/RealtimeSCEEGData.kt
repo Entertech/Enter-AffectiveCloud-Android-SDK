635ed1646be7de64d0fffe3bfa6e68d07f2a8d66
wangkang <1226426603@qq.com>
2023-11-13 10:06:06 +0800

    注释

--- affective_sdk_api/src/main/java/cn/entertech/affective/sdk/bean/RealtimeSCEEGData.kt
+++ affective_sdk_api/src/main/java/cn/entertech/affective/sdk/bean/RealtimeSCEEGData.kt
@@ -1,11 +1,21 @@
 package cn.entertech.affective.sdk.bean
 
 data class RealtimeSCEEGData(
+    /**
+     * 经过滤波后的通道实时脑电波，一个数组，长度为150，对应0.6秒内的脑电波形；
+     * 数值范围[-500, 500]，信号质量不佳时全为0
+     * */
     val sceegWave: List<Double> = emptyList(),
+    //5种脑电波节律的能量分贝值：α波、β波、θ波、δ波、γ波
+    //5种脑电波节律各一个数值，数值范围[0, +∞)，初始阶段与信号质量不佳时返回为0
     val sceegAlphaPower: Double = 0.0,
     val sceegBetaPower: Double = 0.0,
     val sceegThetaPower: Double = 0.0,
     val sceegDeltaPower: Double = 0.0,
     val sceegGammaPower: Double = 0.0,
+    /**
+     * 脑电信号质量等级
+     * 大于1表示脑电信号质量良好
+     * */
     val sceegQuality: Double = 0.0,
 )

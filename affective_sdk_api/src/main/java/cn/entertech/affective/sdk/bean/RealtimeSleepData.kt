3bd1efd40906984e7979bca8758b58b71239502a
wangkang <1226426603@qq.com>
2023-12-26 11:26:34 +0800

    同步新版本sdk中 实时sleep数据结构

--- affective_sdk_api/src/main/java/cn/entertech/affective/sdk/bean/RealtimeSleepData.kt
+++ affective_sdk_api/src/main/java/cn/entertech/affective/sdk/bean/RealtimeSleepData.kt
@@ -1,6 +1,5 @@
 package cn.entertech.affective.sdk.bean
 
-import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
 data class RealtimeSleepData(
@@ -9,13 +8,34 @@ data class RealtimeSleepData(
      * 实时计算的睡眠程度未经过总体校正，因此在清醒、浅睡与深睡之间没有固定的界限划分，但睡眠程度的相对变化仍可实时反映睡眠状态倾向）
      * 数值范围[0, 100],初始化阶段该值为0，初始化完成后的值有效
      * */
-    @SerializedName("sleepDegree") var sleepDegree: Double? = null,
+    var sleepDegree: Double = 0.0,
     /**
      * 实时睡眠状态判断,可用于控制其他设备在入睡后切换状态
      * 数值范围{0, 1}；未睡着之前为0，入睡后保持为1；数组中数值范围[-500, 500]，信号质量不佳时全为0
      * */
-    @SerializedName("sleepState") var sleepState: Double? = null
-) : Serializable {
+    var sleepState: Double = 0.0,
+
+    /**
+     * 睡眠抗干扰,[0,100]
+     * */
+    var sleepSpindle: Double = 0.0,
+
+    /**
+     * 是否更新
+     * */
+    var updateFlag: Boolean = false,
+
+    /**
+     * WAKE = 0  # 清醒
+     * NREM1 = 1  # 非快速眼动期1（思睡期）
+     * NREM2 = 2  # 非快速眼动期2（浅睡期）
+     * NREM3 = 3  # 非快速眼动期3（深睡期）
+     * REM = 4  # 快速眼动期
+     */
+    var sleepStage: Int = 0,
+
+
+    ) : Serializable {
 
     override fun toString(): String {
         return "RealtimeSleepData(sleepDegree=$sleepDegree, sleepState=$sleepState)"
